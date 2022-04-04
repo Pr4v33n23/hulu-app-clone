a9ca35c60201cd465fe154561228a7ffcdbecd09
Pr4v33n23 <praveen.jaisankar@gmail.com>
2022-04-04 18:55:10 +0530

    final commit

--- components/movies/Movies.tsx
+++ components/movies/Movies.tsx
@@ -8,7 +8,7 @@ interface IMoviesProp {
 
 export const Movies: React.FC<IMoviesProp> = ({ movies }) => {
   return (
-    <div>
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {movies.map((movie: Movie) => (
         <MovieThumbnail key={movie.id} movie={movie} />
       ))}
--- components/movies/MoviesThumbnail.tsx
+++ components/movies/MoviesThumbnail.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 import React from "react";
 import { Movie } from "../../types/movie";
-
+import { ThumbUpIcon } from "@heroicons/react/outline";
 
 interface IMovieThumbnailProps {
-    movie: Movie
+  movie: Movie;
 }
 
-export const MovieThumbnail: React.FC<IMovieThumbnailProps> = ({movie}) => {
-console.log(movie);
+export const MovieThumbnail: React.FC<IMovieThumbnailProps> = ({ movie }) => {
+  console.log(movie);
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div>
+    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -22,6 +22,17 @@ console.log(movie);
         width={1920}
         alt="thumbnail"
       />
+      <div>
+        <p className="truncate max-w-md">{movie.overview}</p>
+        <h2 className="mt-1 text-2xl text-white transition-all duration-100 ease-in-out group-hover:font-bold">
+          {movie.title || movie.original_name}
+        </h2>
+        <p className="flex items-center opacity-0 group-hover:opacity-100">
+          {movie.media_type && `${movie.media_type} •`}{" "}
+          {movie.release_date || movie.first_air_date} •{" "}
+          <ThumbUpIcon className="h-5 mx-2" /> {movie.vote_count}
+        </p>
+      </div>
     </div>
   );
 };
